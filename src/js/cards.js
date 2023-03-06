57186d396d8c85903c8bc261d2b4c697b6a0c2cf
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-03-06 17:13:59 +0300

    fix

---
+++ src/js/cards.js
@@ -0,0 +1,11 @@
+const btnPlus = document.querySelector('.collection__btn-plus')
+
+btnPlus.addEventListener('click', (e) => {
+  const itemMenuImage = document.querySelector(`.card[data-name=${e.target.dataset.name}]`);
+  console.log(itemMenuImage)
+  const allItem = document.querySelectorAll('.card');
+  allItem.forEach(item => {
+  item.classList.remove('card-active')
+  })
+  itemMenuImage.classList.add('card-active')
+})

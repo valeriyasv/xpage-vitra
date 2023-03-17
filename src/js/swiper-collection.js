64be19a7d74ce5fcb023944c723e398da8826e8b
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-03-18 01:40:25 +0300

    fix

--- src/js/swiper-collection.js
+++ src/js/swiper-collection.js
@@ -1,19 +1,18 @@
 const swiper2 = new Swiper('.swiper-container-card', {
   // Options go here
+ 
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
   loop: true,
-  
+
   slidesPerView: 1,
-  
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
-
-
 
 console.log(swiper2, 'swiper2')

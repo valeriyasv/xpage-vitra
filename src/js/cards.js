bb7ac72baea6ccdd7a0834161bbb2d849dcdbf82
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-03-20 02:56:56 +0300

    fix

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,19 +1,22 @@
-const btnPlus = document.querySelector('.collection__btn-plus')
+const btnPlus = document.querySelectorAll('.collection__btn-plus')
 
+btnPlus.forEach((item)  => {
 
-btnPlus.addEventListener('click', (e) => {
-  const itemMenuImage = document.querySelector(`.card[data-name=${e.target.dataset.name}]`);
-  console.log(itemMenuImage)
-  const allItem = document.querySelectorAll('.card');
-  allItem.forEach(item => {
-    item.classList.remove('card-active')
-  })
+  item.addEventListener('click', (e) => {
+    const itemMenuImage = document.querySelector(`.card[data-name=${e.target.dataset.name}]`);
+    console.log(itemMenuImage)
+    const allItem = document.querySelectorAll('.card');
+    allItem.forEach(item => {
+      item.classList.remove('card-active')
+    })
   itemMenuImage.classList.add('card-active')
   const activeBtn = document.querySelector(`.collection__btn-plus-black[data-name=${e.target.dataset.name}]`);
-
+  
   const allBtn = document.querySelectorAll('.collection__btn-plus-black');
   allBtn.forEach(item => {
     item.classList.remove('black-active')
   })
   activeBtn.classList.add('black-active');
+})
+
 })
--- src/js/search.js
+++ src/js/search.js
@@ -1,12 +1,27 @@
 const inputSearch = document.querySelector('.main-header__search-text');
 const search = document.querySelector('.main-header__search--icon');
+const logo = document.querySelector('.main-header__logo');
 
-
+function resize() {
+  if (window.innerWidth < 500) {
+    // Изменение стилей для мобильных устройств
+    logo.style.display = 'none';
+    console.log('logo')
+  } 
+}
+  
 search.addEventListener('click', () => { 
   search.classList.add('search-active');
+  resize();
   document.body.addEventListener('click', (e) => {
     if (!e.target.classList.contains('main-header__search-text') && !e.target.classList.contains('main-header__search--icon') && !e.target.classList.contains('main-header__search--btn') && !e.target.classList.contains('main-header__search-placehold') && !e.target.classList.contains('search-svg'))  {
       search.classList.remove('search-active');
+
+      if (window.innerWidth < 500) {
+        // Изменение стилей для мобильных устройств
+        logo.style.display = 'block';
+        console.log('logo')
+      }
     }
   })
 })

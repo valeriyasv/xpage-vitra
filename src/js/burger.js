1b98e2c1c430f312af22c6fc7a88dbbd2b084248
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-03-23 02:20:16 +0300

    fix

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,21 +2,21 @@ const btn = document.querySelector('.main-header__burger');
 const menu = document.querySelector('.menu');
 const closeBurger = document.querySelector('.main-header__burger-close');
 const santehLink = document.querySelector('.menu__santeh-link');
-const santehMob = document.querySelector('.menu-santeh');
+const santehMob = document.querySelector('.menu__shell');
 
 btn.addEventListener('click', () => {
   menu.style.display = 'block';
   closeBurger.style.display = 'block';
   btn.style.display = 'none';
 })
+santehLink.addEventListener('click', () => {
+  console.log(santehMob)
+  santehMob.style.display = 'block';
+  menu.style.display = 'none';
+})
 
 closeBurger.addEventListener('click', () => {
   closeBurger.style.display = 'none';
   btn.style.display = 'block';
-  menu.style.display = 'none';
-})
-
-santehLink.addEventListener('click', () => {
-  santehMob.style.display = 'block';
   menu.style.display = 'none';
 })

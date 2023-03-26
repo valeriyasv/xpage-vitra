145d71d835741511df4e4581057e9eb3d57c5815
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-03-27 00:58:56 +0300

    fix

--- src/js/search.js
+++ src/js/search.js
@@ -3,7 +3,7 @@ const search = document.querySelector('.main-header__search--icon');
 const logo = document.querySelector('.main-header__logo');
 
 function resize() {
-  if (window.innerWidth < 500) {
+  if (window.innerWidth < 600) {
     // Изменение стилей для мобильных устройств
     logo.style.display = 'none';
     console.log('logo')
@@ -17,7 +17,7 @@ search.addEventListener('click', () => {
     if (!e.target.classList.contains('main-header__search-text') && !e.target.classList.contains('main-header__search--icon') && !e.target.classList.contains('main-header__search--btn') && !e.target.classList.contains('main-header__search-placehold') && !e.target.classList.contains('search-svg'))  {
       search.classList.remove('search-active');
 
-      if (window.innerWidth < 500) {
+      if (window.innerWidth < 600) {
         // Изменение стилей для мобильных устройств
         logo.style.display = 'block';
         console.log('logo')

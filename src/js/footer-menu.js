36ca564a78f5529576f562ab6114522ad41be6a5
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-03-29 22:46:28 +0300

    fix

---
+++ src/js/footer-menu.js
@@ -0,0 +1,7 @@
+const title = document.querySelector('.footer__nav-item--title');
+const item = document.querySelector('.footer__nav-item');
+
+title.addEventListener('click', () => {
+  item.style.display = 'block';
+  console.log('hhhh')
+})

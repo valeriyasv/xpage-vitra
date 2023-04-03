d08809b516b539b591a954c3dfc4dc57f0d3c291
Valeriya Savchenko <lerushka.sav@gmail.com>
2023-04-03 10:43:22 +0300

    fix

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,24 +1,28 @@
 const btn = document.querySelector('.main-header__burger');
 const menu = document.querySelector('.menu');
 const closeBurger = document.querySelector('.main-header__burger-close');
-const santehLink = document.querySelector('.menu__santeh-link');
-const santehMob = document.querySelector('.menu__shell');
+const santeh = document.querySelector('.menu__picture');
+const santehList = document.querySelector('.menu-santeh');
+const menuSan = document.querySelector('.menu__shell');
+
 
 btn.addEventListener('click', () => {
   menu.style.display = 'block';
-  closeBurger.style.display = 'block';
+  menu.style.transform = 'translateX(100%)';
+  closeBurger.style.display = 'flex';
   btn.style.display = 'none';
-  menu.style.display = 'flex';
-  menu.style.transform = menu.style.transform === 'translateX(0)' ? 'translateX(100%)' : 'translateX(0)';
-})
-santehLink.addEventListener('click', () => {
-  console.log(santehMob)
-  santehMob.style.display = 'block';
-  menu.style.display = 'none';
 })
 
 closeBurger.addEventListener('click', () => {
   closeBurger.style.display = 'none';
-  btn.style.display = 'block';
-  menu.style.display = 'none';
+  btn.style.display = 'flex';
+  menu.style.transform = '';
+  santehList.style.transform = '';
+})
+
+santeh.addEventListener('click', () => {
+  santehList.style.display = 'block';
+  santehList.style.transform = 'translateX(100%)';
+  menu.style.transform = '';
+  console.log('kkkkkk')
 })
--- src/js/footer-menu.js
+++ src/js/footer-menu.js
@@ -1,7 +1,26 @@
-const title = document.querySelector('.footer__nav-item--title');
-const item = document.querySelector('.footer__nav-item');
+const titlecompany = document.querySelector('.footer__nav-item--company');
+const itemCompany = document.querySelectorAll('.footer__nav-company');
 
-title.addEventListener('click', () => {
-  item.style.display = 'block';
-  console.log('hhhh')
+const titleProducts = document.querySelector('.footer__nav-item--products');
+const itemProducts = document.querySelectorAll('.footer__nav-products');
+
+const titleStore = document.querySelector('.footer__nav-item--store');
+const itemStore = document.querySelectorAll('.footer__nav-store');
+
+titlecompany.addEventListener('click', () => {
+    itemCompany.forEach(item => {
+    item.style.display = 'block';
+  })
+})
+
+titleProducts.addEventListener('click', () => {
+  itemProducts.forEach(item => {
+    item.style.display = 'block';
+  })
+})
+
+titleStore.addEventListener('click', () => {
+  itemStore.forEach(item => {
+    item.style.display = 'block';
+  })
 })
